e0786abaf702b9a34c208e13a8dd5cd2c5cb76e8
senaps <gerdakan.sa@gmail.com>
2019-03-20 02:33:27 +0330

    variable declaration and value assign

--- test.go
+++ test.go
@@ -4,17 +4,32 @@ import(
 	"fmt"
 )
 
-
-/*func testfunc(){
-	fmt.Println("hello wrold from a function:)")
+/*
+func main(){
+	fmt.Println("hello world!")
 }
 */
+
+/*
+func testfunc(){
+	fmt.Println("hello wrold from a function:)")
+}
+func main(){
+    testfunc()
+}
+*/
+
 /*
 func addnumbers(first int, second int) int{
 	return first + second
 }
+
+func main(){
+    fmt.Println(addnumbers(5,6))
+}
 */
 
+/*
 func addNumbers(first, second int) (sum int, errString string){
 	// we have added names for return shits...
 	sum = first + second
@@ -23,8 +38,17 @@ func addNumbers(first, second int) (sum int, errString string){
 }
 
 func main(){
-	//fmt.Println("hello world!:)")
-	//testfunc()
 	result, some := addNumbers(5, 6)  // we need to use := wtf is the difference?
 	fmt.Println(result, some)
+}
+*/
+
+var name, family string  // this how we make variables!
+
+func main(){
+	name = "maysam"  // this how we assign data to a variable
+	var age int = 27  // this how we make a variable with data 
+	//address = "Tehran"  // this wont work!
+	var address = "Tehran"  //this works! so no need of the variable type
+	fmt.Println(name, family, age, address)
 }
